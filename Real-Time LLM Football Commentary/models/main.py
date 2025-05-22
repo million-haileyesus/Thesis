2802c2388d5335d7afdaa2f0f4d103e648fdb6a8
Million <noillimsey@gmail.com>
2025-05-21 21:47:55 -0400

    Added model testing and incorporating hyperparameters used for a model training in the plots

--- Real-Time LLM Football Commentary/models/main.py
+++ Real-Time LLM Football Commentary/models/main.py
@@ -1,3 +1,5 @@
+import inspect
+
 from .lstm import LSTM
 from .neural_network import NeuralNetwork
 from .optimizer_utils import get_optimizer, get_scheduler
@@ -8,7 +10,15 @@ def get_model(model, params=None):
     if model:
         if params is None:
             raise ValueError("Parameters for 'NeuralNetwork' must be provided in params.")
-        return model(**params)
+
+        expected_params = inspect.signature(model.__init__).parameters.keys()
+        # Remove 'self' which is always the first parameter
+        expected_params = [p for p in expected_params if p != 'self']
+        
+        # Filter the params dictionary to only include expected parameters
+        filtered_params = {k: params[k] for k in expected_params if k in params}
+        
+        return model(**filtered_params)
     else:
         raise ValueError("Unsupported model type. Use the correct model.")
 
@@ -43,3 +53,16 @@ def train_model(model, train_loader, validation_loader, epochs, optimizer_name, 
                   f"Precision: {val_metrics['precision'] * 100:.2f}% | Recall: {val_metrics['recall'] * 100:.2f}% | F1: {val_metrics['f1'] * 100:.2f}%")
 
     return history, train_metrics["train_labels"], train_metrics["train_preds"], val_metrics["val_labels"], val_metrics["val_preds"]
+
+
+def test_model(model, validation_loader, criterion, device, verbose=True, is_rnn=False, is_gnn=False, is_transformer=False):
+    history = {"validation_accuracy": []}
+    val_loss, val_acc, val_metrics = validate_one_epoch(model, validation_loader, criterion, device, is_rnn=is_rnn, is_gnn=is_gnn, is_transformer=is_transformer)
+    history["validation_accuracy"].append(val_acc)
+
+    if verbose:
+        print(f"Val accuracy: {val_acc * 100:.2f}% | "
+              f"Val loss: {val_loss:.4f} | "
+              f"Precision: {val_metrics['precision'] * 100:.2f}% | Recall: {val_metrics['recall'] * 100:.2f}% | F1: {val_metrics['f1'] * 100:.2f}%")
+
+    return history, val_metrics["val_labels"], val_metrics["val_preds"]
